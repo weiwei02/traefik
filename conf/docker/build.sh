8178010778ac453a4a75e652366d653d8d866c1b
weiwei3.wang <weiwei3.wang@ly.com>
2020-06-15 20:46:25 +0800

    modiffy log path

--- conf/docker/build.sh
+++ conf/docker/build.sh
@@ -1,1 +1,5 @@
-docker build -t hub.17usoft.com/cncf/traefik_proxy -f conf/docker/Dockerfile ./
+ rm -rf static/ autogen/; make generate-webui
+ go generate
+ docker build -t hub.17usoft.com/cncf/traefik_proxy:1.0.1 -f conf/docker/Dockerfile ./
+ docker push hub.17usoft.com/cncf/traefik_proxy:1.0
+ docker tag hub.17usoft.com/cncf/traefik_proxy hub.17usoft.com/cncf/traefik_proxy:1.0
