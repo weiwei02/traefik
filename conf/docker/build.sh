62694b911340d8dddfbe34ec02899a11b9f82ce9
weiwei3.wang <weiwei3.wang@ly.com>
2020-06-22 20:47:22 +0800

    增加生产部署脚本

--- conf/docker/build.sh
+++ conf/docker/build.sh
@@ -1,5 +1,6 @@
  rm -rf static/ autogen/; make generate-webui
  go generate
+ script/binary
  docker build -t hub.17usoft.com/cncf/traefik_proxy:1.0.1 -f conf/docker/Dockerfile ./
  docker push hub.17usoft.com/cncf/traefik_proxy:1.0
  docker tag hub.17usoft.com/cncf/traefik_proxy hub.17usoft.com/cncf/traefik_proxy:1.0
